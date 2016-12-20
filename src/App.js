2b6efc4a36a736735da26ec3afc520595cf51280
RadoszSzymon <radoszszymon@gmail.com>
2016-12-20 15:22:19 +0100

    finished logic of app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects from './Components/Projects'
+import AddProject from './Components/AddProject'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index,1);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-        TasksApp
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
